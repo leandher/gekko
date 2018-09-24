1a3c06219dc8398d69cbfe3c00e561e1767141d8
leandher <leandher_bessa@hotmail.com>
2018-09-24 18:38:11 -0300

    Update UIconfig.js

--- web/vue/public/UIconfig.js
+++ web/vue/public/UIconfig.js
@@ -5,22 +5,22 @@
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
 const CONFIG = {
-  headless: false,
-  api: {
-    host: '0.0.0.0',
-    port: 3000,
-    timeout: 120000 // 2 minutes
-  },
-  ui: {
-    ssl: true,
-    host: 'young-harbor-76195.herokuapp.com',
-    port: 80,
-    path: '/'
-  },
-  adapter: 'sqlite'
-}
-
-if(typeof window === 'undefined')
-  module.exports = CONFIG;
-else
-  window.CONFIG = CONFIG;
+headless: true,
+api: {
+host: '0.0.0.0',
+port:
+typeof process !== 'undefined' &&
+process !== undefined &&
+process.env.PORT
+? process.env.PORT
+: 8080,
+timeout: 120000, // 2 minutes
+},
+ui: {
+ssl: true,
+host: 'https://young-harbor-76195.herokuapp.com',
+port: '443',
+path: '/',
+},
+adapter: 'sqlite',
+};
