76b7ec41999dd2175dcd082f2cf14c0ff8335a76
leandher <leandher_bessa@hotmail.com>
2018-09-24 17:45:28 -0300

    config

--- web/vue/public/UIconfig.js
+++ web/vue/public/UIconfig.js
@@ -7,14 +7,14 @@
 const CONFIG = {
   headless: false,
   api: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 3000,
     timeout: 120000 // 2 minutes
   },
   ui: {
-    ssl: false,
-    host: 'localhost',
-    port: 3000,
+    ssl: true,
+    host: 'young-harbor-76195.herokuapp.com',
+    port: 80,
     path: '/'
   },
   adapter: 'sqlite'
