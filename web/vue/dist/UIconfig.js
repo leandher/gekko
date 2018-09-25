d5a988aa4fe321538e8519fecc360f06975d24eb
leandher <leandher_bessa@hotmail.com>
2018-09-25 09:49:47 -0300

    localhost

--- web/vue/dist/UIconfig.js
+++ web/vue/dist/UIconfig.js
@@ -5,36 +5,36 @@
 // see https://gekko.wizb.it/docs/installation/installing_gekko_on_a_server.html#Configuring-Gekko
 
 // Prod
-const CONFIG = {
-  headless: true,
-  api: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
-  ui: {
-    ssl: false,
-    host: 'young-harbor-76195.herokuapp.com',
-    port: 80,
-    path: '/',
-  },
-  adapter: 'sqlite',
-};
-
-// Dev
 // const CONFIG = {
-//   headless: false,
+//   headless: true,
 //   api: {
-//     host: '127.0.0.1',
+//     host: '0.0.0.0',
 //     port: 3000,
-//     timeout: 120000, // 2 minutes
 //   },
 //   ui: {
 //     ssl: false,
-//     host: 'localhost',
-//     port: 3000,
+//     host: 'young-harbor-76195.herokuapp.com',
+//     port: 80,
 //     path: '/',
 //   },
 //   adapter: 'sqlite',
 // };
+
+// Dev
+const CONFIG = {
+  headless: false,
+  api: {
+    host: '127.0.0.1',
+    port: 3000,
+    timeout: 120000, // 2 minutes
+  },
+  ui: {
+    ssl: false,
+    host: 'localhost',
+    port: 3000,
+    path: '/',
+  },
+  adapter: 'sqlite',
+};
 if (typeof window === 'undefined') module.exports = CONFIG;
 else window.CONFIG = CONFIG;
